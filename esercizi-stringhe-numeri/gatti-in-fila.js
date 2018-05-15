e1c06575b98e84253289afa1aac97f710276bf0b
thevinsohe <39291874+thevinsohe@users.noreply.github.com>
2018-05-15 08:02:14 +0200

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -12,3 +12,4 @@
   Hint: Se vuoi usare l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su) ma utilizza Math.ceil (arrotonda sempre all'intero superiore).
   http://www.imparareaprogrammare.it
 */
+var = "bagheera"
